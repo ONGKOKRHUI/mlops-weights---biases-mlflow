4299b398ba0805af710f510a4dcc8aacf7852c5d
Vincent Ong Kok Rhui <133461913+ONGKOKRHUI@users.noreply.github.com>
2025-12-30 14:25:45 +0800

    Remove unused MNIST dataset files and add initial configuration and structure for the project

---
+++ config.py
@@ -0,0 +1,10 @@
+# Hyperparameter configuration
+PARAMS = {
+    "epochs": 5,
+    "classes": 10,
+    "kernels": [16, 32],
+    "batch_size": 128,
+    "learning_rate": 0.005,
+    "dataset": "MNIST",
+    "architecture": "CNN"
+}
